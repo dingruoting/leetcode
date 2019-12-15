51775a2f1006f6b6b58878c746288d223f98b6a5
xiaoxin <dingruoting0103@gmail.com>
2019-12-16 00:16:59 +0800

    update

--- include/RandomList.h
+++ include/RandomList.h
@@ -21,7 +21,7 @@ class Node {
 };
 class RandomList : public Solution {
  public:
-//  给定一个链表，每个节点包含一个额外增加的随机指针，
+//  蚂蚁金服面试题：给定一个链表，每个节点包含一个额外增加的随机指针，
 //  该指针可以指向链表中的任何节点或空节点。
 //  要求返回这个链表的深拷贝。
   Node *copyRandomList(Node *head) {
--- include/VectorSolution.h
+++ include/VectorSolution.h
@@ -21,10 +21,9 @@ private:
     void rotate(vector<vector<int>> &matrix) {
         //\todo
     }
-//    给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+//    阿里云 面试题 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
 //    如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
 //    注意你不能在买入股票前卖出股票。
-
     int maxProfit(vector<int> &prices) {
         if (prices.empty()) {
             return 0;
