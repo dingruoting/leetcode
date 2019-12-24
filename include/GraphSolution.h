40f71815660d937207338cae220d31659ef7deb9
ruoting <ruoting.ding@hobot.cc>
2019-12-24 16:06:42 +0800

    graph2

--- include/GraphSolution.h
+++ include/GraphSolution.h
@@ -28,6 +28,33 @@ class GraphSolution : public Solution {
     }
     return -1;
   }
+//  有 N 个花园，按从 1 到 N 标记。在每个花园中，你打算种下四种花之一。
+//  paths[i] = [x, y] 描述了花园 x 到花园 y 的双向路径。
+//  另外，没有花园有 3 条以上的路径可以进入或者离开。
+//  你需要为每个花园选择一种花，使得通过路径相连的任何两个花园中的花的种类互不相同。
+//  以数组形式返回选择的方案作为答案 answer，其中 answer[i] 为在第 (i+1) 个花园中种植的花的种类。花的种类用  1, 2, 3, 4 表示。保证存在答案。
+//  分析：限制每个节点的度为3，同时提供四种颜色，因此不需要回溯
+  vector<int> gardenNoAdj(int N, vector<vector<int>> &paths) {
+    // 1 构建邻接表
+    std::vector<std::vector<int>> table(N);
+    for (const auto &path: paths) {
+      if (path[1] < path[0]) {
+        table[path[0] - 1].emplace_back(path[1] - 1);
+      } else {
+        table[path[1] - 1].emplace_back(path[0] - 1);
+      }
+    }
+    // 2 遍历所有节点，对于每个节点 查看其邻接点颜色，使用不同的颜色染色即可
+    std::vector<int> color(N, 1);
+    for (auto i = 1; i < N; ++i) {
+      set<int> all_colors = {1, 2, 3, 4};
+      for (const auto &adj : table[i]) {
+        all_colors.erase(color[adj]);
+      }
+      color[i] = *all_colors.begin();
+    }
+    return color;
+  }
   void Test() override;
 };
 
--- src/GraphSolution.cpp
+++ src/GraphSolution.cpp
@@ -5,12 +5,25 @@
 #include "../include/GraphSolution.h"
 
 void GraphSolution::Test() {
-  int num = 2;
-  vector<vector<int>> trust = {{1, 2}};
-  auto ans = findJudge(num, trust);
-  std::cout << "judge is " << ans << std::endl;
-  num = 3;
-  trust = {{1, 3},{2,3},{3,1}};
-  ans = findJudge(num, trust);
-  std::cout << "judge is " << ans << std::endl;
+//  int num = 2;
+//  vector<vector<int>> trust = {{1, 2}};
+//  auto ans = findJudge(num, trust);
+//  std::cout << "judge is " << ans << std::endl;
+//  num = 3;
+//  trust = {{1, 3},{2,3},{3,1}};
+//  ans = findJudge(num, trust);
+//  std::cout << "judge is " << ans << std::endl;
+  int garden_num = 4;
+  vector<vector<int>> garden_path = {{1, 2}, {3, 4}};
+  auto garden_color = gardenNoAdj(garden_num, garden_path);
+  for (const auto &color : garden_color) {
+    std::cout << color << " ";
+  }
+  std::cout << std::endl;
+  garden_path = {{1, 2}, {2, 3}, {3, 4}, {4, 1}, {1, 3}, {2, 4}};
+  garden_color = gardenNoAdj(garden_num, garden_path);
+  for (const auto &color : garden_color) {
+    std::cout << color << " ";
+  }
+  std::cout << std::endl;
 }
