08550b3284cf06e4de71b15d4bbda99426a249fb
ruoting <ruoting.ding@hobot.cc>
2019-12-24 14:39:38 +0800

    intersection

--- src/VectorSolution.cpp
+++ src/VectorSolution.cpp
@@ -14,8 +14,8 @@ void PrintVectorVector(vector<vector<int>> datas) {
 }
 
 void VectorSolution::Test() {
-//    vector<int> data1 = {1,2,3};
-//    vector<int> data2 = {4,5,6};
+  vector<int> data1 = {1, 66, 66,3};
+  vector<int> data2 = {66, 5, 68,66};
 //    vector<int> data3 = {7,8,9};
 //  vector<vector<int>> testdata = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 //  rotate(testdata);
@@ -26,11 +26,16 @@ void VectorSolution::Test() {
 //  std::cout << "search " << search(data, 0) << std::endl;
 //  vector<int> data = {1, 2, 5, 6, 7};
 //  std::cout << "search " << searchInOrder(data, 6) << std::endl;
+//
+//  std::vector<std::vector<int>> inputs =
+//      {{1, 2, 3, 9, 10}, {3, 4, 11}, {2, 3, 4, 9, 10}, {2, 3, 5, 8, 9}};
+//  auto ans = findCommonElems1(inputs);
+//  for (auto i = 0; i < ans.size(); ++i) {
+//    cout << ans[i] << endl;
+//  }
 
-  std::vector<std::vector<int>> inputs = {{1,2,3,9,10},{3,4,11},{2,3,4,9,10},{2,3,5,8,9}};
-  auto ans = findCommonElems1(inputs);
-  for(auto i = 0; i < ans.size();++i){
+  auto ans = intersection2(data1, data2);
+  for (auto i = 0; i < ans.size(); ++i) {
     cout << ans[i] << endl;
   }
-
 }
