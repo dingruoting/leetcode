4a99e27f9a5c7867da607983384fc0b82fb9c10c
ruoting <ruoting.ding@hobot.cc>
2019-12-25 17:46:24 +0800

    find min

--- include/SearchSolution.h
+++ include/SearchSolution.h
@@ -9,12 +9,11 @@ class SearchSolution : public Solution {
  public:
   void Test() override;
  private:
-  // 二分查找
-  // 给定一个排序数组和一个目标值，在数组中找到目标值，
-  // 并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
-  int BinarySearchInsert(vector<int> &nums, int target) {
-    int p = 0;
-    int q = nums.size() - 1;
+  int BinarySearch(vector<int> &nums,
+                   int target,
+                   int p,
+                   int q,
+                   bool search_insert = false) {
     while (p <= q) {
       auto mid = (p + q) >> 1;
       if (nums[mid] == target) {
@@ -25,7 +24,17 @@ class SearchSolution : public Solution {
         q = mid - 1;
       }
     };
-    return p;
+    if (search_insert) {
+      return p;
+    } else {
+      return -1;
+    }
+  }
+  // 二分查找
+  // 给定一个排序数组和一个目标值，在数组中找到目标值，
+  // 并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+  int BinarySearch(vector<int> &nums, int target) {
+    return BinarySearch(nums, target, 0, nums.size() - 1, true);
   }
 //  计算并返回 x 的平方根，其中 x 是非负整数。
 //  由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
@@ -88,14 +97,92 @@ class SearchSolution : public Solution {
     auto right = searchBound(nums, target, false);
     return {left, right};
   }
+
+  // 旋转数组的最小值
+  int findMin(vector<int> &nums) {
+    if (nums.empty()) {
+      return -1;
+    }
+    int p = 0;
+    int q = nums.size() - 1;
+    // 不断缩小检查范围，直至p = q，区间只剩下一个值。
+    while (p < q) {
+      int mid = (p + q) / 2;
+      const int &final_val = nums[q];
+      const int &mid_val = nums[mid];
+      if (mid_val > final_val) {
+        // mid在第一个排序数组里，最小值在后面
+        p = mid + 1;
+      } else if (mid_val < final_val) {
+        // mid在第二个排序数组里，最小值在该区间范围内,包括自己
+        q = mid;
+      } else {
+        // 无重复数字时，永远进入不到该条件里。
+        // 有重复数字时，无法判断该点属于哪个区间范围，通过将最后一个点排除掉来缩小范围
+        q = q - 1;
+      }
+    }
+    return nums[p];
+  }
+
   // 今日头条面试题
 //  假设按照升序排序的数组在预先未知的某个点上进行了旋转。
 //  ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
 //  搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
 //  你可以假设数组中不存在重复的元素。
-//  你的算法时间复杂度必须是 O(log n) 级别。
-  void printTestResult(vector<int> data, int target);
-
+  int search1(vector<int> &nums, int target) {
+    if (nums.empty()) {
+      return -1;
+    }
+    if (nums.size() == 1) {
+      return nums[0] == target ? 0 : -1;
+    }
+    auto rotate_idx = findMin(nums);
+    if (rotate_idx == 0) {
+      return BinarySearch(nums, target, 0, nums.size() - 1);
+    } else {
+      if (nums[0] <= target) {
+        return BinarySearch(nums, target, 0, rotate_idx - 1);
+      } else {
+        return BinarySearch(nums, target, rotate_idx, nums.size() - 1);
+      }
+    }
+  }
+  int search2(vector<int> &nums, int target) {
+    if (nums.empty()) {
+      return -1;
+    }
+    if (nums.size() == 1) {
+      return nums[0] == target ? 0 : -1;
+    }
+    int p = 0;
+    int q = nums.size() - 1;
+    while (p <= q) {
+      auto mid = (p + q) / 2;
+      const auto &mid_val = nums[mid];
+      if (mid_val == target) {
+        return mid;
+      } else {
+        // mid之前是有序的，判断目标是否在升序范围内
+        if (mid_val >= nums[p]) {
+          if (nums[p] <= target && mid_val > target) {
+            q = mid - 1;
+          } else {
+            p = mid + 1;
+          }
+        } else {
+          // mid之后是有序的
+          if (mid_val < target && nums[q] >= target) {
+            p = mid + 1;
+          } else {
+            q = mid - 1;
+          }
+        }
+      }
+    }
+    return -1;
+  }
+  void printTestResult(vector<int> data, int target, int);
 };
 
 #endif //CODING_SEARCHSOLUTION_H
--- src/SearchSolution.cpp
+++ src/SearchSolution.cpp
@@ -4,23 +4,38 @@
 
 #include "../include/SearchSolution.h"
 
-void SearchSolution::printTestResult(vector<int> data, int target) {
+void SearchSolution::printTestResult(vector<int> data,
+                                     int target,
+                                     int expect_res) {
 //  std::cout << "search result for " << target << " in {";
 //  for (const auto &val : data) {
 //    std::cout << val << " ";
 //  }
 //  std::cout << "} is " << BinarySearchInsert(data, target) << std::endl;
+//  auto res = search(data, target);
+//  std::cout << "} is " << search(data, target);
 
-  std::cout << "search result for " << target << " in {";
+
+  std::cout << "min val in {";
   for (const auto &val : data) {
-    std::cout << " " << val;
+    std::cout << val << " ";
   }
-  std::cout << " } is {";
-  auto ans = searchRange(data, target);
-  for (const auto &val : ans) {
-    std::cout << " " << val;
+  auto ans = findMin(data);
+  std::cout << "} is " << ans << std::endl;
+  if (ans != expect_res) {
+    std::cout << " result is failed!!";
   }
-  std::cout << " }" << std::endl;
+  std::cout << std::endl;
+//  std::cout << "search result for " << target << " in {";
+//  for (const auto &val : data) {
+//    std::cout << " " << val;
+//  }
+//  std::cout << " } is {";
+//  auto ans = searchRange(data, target);
+//  for (const auto &val : ans) {
+//    std::cout << " " << val;
+//  }
+//  std::cout << " }" << std::endl;
 }
 
 void SearchSolution::Test() {
@@ -41,6 +56,27 @@ void SearchSolution::Test() {
 //  std::cout << "my sqrt for " << data << " is " << mySqrt(data) << std::endl;
 //  printTestResult({5, 7, 7, 8,8,10}, 8);
 //  printTestResult({5, 7, 7, 8,8,10}, 6);
-  printTestResult({}, 0);
+//  printTestResult({4, 5, 6, 7, 0, 2, 3}, 3, 6);
+//  printTestResult({4, 5, 6, 7, 0, 2, 3}, 0, 4);
+//  printTestResult({2, 5, 6, 7, 0, 1}, 2, 0);
+//  printTestResult({6, 7, 8, 1, 2, 3, 4, 5}, 6, 0);
+//  printTestResult({2, 5, 6, 7, 0, 1}, 8, -1);
+//  printTestResult({8, 11, 22, 33, 0, 3, 6}, 5, -1);
+//  printTestResult({8, 9, 2, 3, 4}, 9, 1);
+//  printTestResult({1, 2, 3, 4, 5, 6}, 3, 2);
+//  printTestResult({1, 2, 3}, 3, 2);
+//  printTestResult({1, 3}, 3, 1);
+//  printTestResult({5, 1, 3}, 3, 2);
+  printTestResult({}, 0, -1);
+  printTestResult({3, 1}, 1, 1);
+
+  printTestResult({8, 9, 2, 3, 4}, 9, 2);
+  printTestResult({1, 2, 3, 4, 5, 6}, 3, 1);
+  printTestResult({1, 2, 3}, 3, 1);
+  printTestResult({2,2,2,0,1}, 3, 0);
+  printTestResult({3,1,3}, 3, 1);
+  printTestResult({3,3,1,3}, 3, 1);
+  printTestResult({}, 0, -1);
+  printTestResult({3, 1, 1}, 1, 1);
 
 }
