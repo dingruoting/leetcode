01ebd0f5bc3ba13d97f7c4e41870659a2b444b75
ruoting <ruoting.ding@hobot.cc>
2019-12-29 20:01:12 +0800

    product

--- include/DynamicProgramming.h
+++ include/DynamicProgramming.h
@@ -6,7 +6,28 @@
 #define CODING_DYNAMICPROGRAMMING_H
 #include "Solution.h"
 class DynamicProgramming : public Solution {
+ public:
   void Test() override;
+ private:
+//  今日头条面试题
+//  给定长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output
+//  其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
+//  说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+// 思路：output[i]为当前数左边的乘积 * 右边的乘积
+  vector<int> productExceptSelf(vector<int> &nums) {
+    vector<int> output(nums.size(), 1);
+    int product_left = 1;
+    int product_right = 1;
+    for (auto left = 0; left < nums.size(); ++left) {
+      auto right = nums.size() - left - 1;
+      output[left] *= product_left;
+      product_left *= nums[left];
+      output[right] *= product_right;
+      product_right *= nums[right];
+    }
+    return output;
+  }
+
 };
 
 #endif //CODING_DYNAMICPROGRAMMING_H
--- src/DynamicProgramming.cpp
+++ src/DynamicProgramming.cpp
@@ -4,5 +4,11 @@
 
 #include "../include/DynamicProgramming.h"
 void DynamicProgramming::Test() {
-  
+  std::vector<int> data = {1,2,3,4};
+  auto ans = productExceptSelf(data);
+  std::cout << "[" ;
+  for(const auto& num : ans){
+    std::cout << num << " ";
+  }
+  std::cout << "] " << std::endl;
 };
