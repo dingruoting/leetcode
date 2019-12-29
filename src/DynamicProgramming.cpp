18dd3852f357c1f07edfa31ca99bdcbf9186711a
xiaoxin <dingruoting0103@gmail.com>
2019-12-29 21:41:54 +0800

    dp rob

--- src/DynamicProgramming.cpp
+++ src/DynamicProgramming.cpp
@@ -14,5 +14,8 @@ void DynamicProgramming::Test() {
 //  std::cout << "] " << std::endl;
 //    std::vector<int> data = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 //    std::cout << "maxSubArray is " << maxSubArray(data) << std::endl;
-    std::cout << "climbStairs is " << climbStairs(4) << std::endl;
+//    std::cout << "climbStairs is " << climbStairs(4) << std::endl;
+//    std::vector<int> data = {2, 7, 9, 3, 1};
+    std::vector<int> data = {2, 1, 1, 2};
+    std::cout << "max rob is " << rob(data) << std::endl;
 };
